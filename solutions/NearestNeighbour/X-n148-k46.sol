0996f4484b8278e856d78d1a0325bad8c7a4414d
MaxUhl98 <frozen2ice@web.de>
2024-06-19 11:08:06 +0200

    push relevant files into main

---
+++ solutions/NearestNeighbour/X-n148-k46.sol
@@ -0,0 +1,55 @@
+Route #1: 99 42 46
+Route #2: 23 47 126
+Route #3: 75 88
+Route #4: 118 62 107
+Route #5: 39 89
+Route #6: 43 58
+Route #7: 44
+Route #8: 86 84 134
+Route #9: 73 53 19
+Route #10: 18 136
+Route #11: 110 137 127
+Route #12: 85 77 142
+Route #13: 21 124 72
+Route #14: 82 104 32
+Route #15: 68 54 10 129
+Route #16: 122 2
+Route #17: 109 17
+Route #18: 93 83
+Route #19: 111 27 22
+Route #20: 106
+Route #21: 14 67 98 61
+Route #22: 112 92 79
+Route #23: 121 140
+Route #24: 123 128
+Route #25: 143 76
+Route #26: 4 114 31 15 108
+Route #27: 56 132
+Route #28: 113 70
+Route #29: 30 45 60 94
+Route #30: 55 49 145 34
+Route #31: 87 95
+Route #32: 35 139
+Route #33: 57 7 130
+Route #34: 96 146
+Route #35: 91 33 36 66
+Route #36: 97 28
+Route #37: 8 125 37 26 20 52
+Route #38: 102 138 64 131
+Route #39: 59 1
+Route #40: 100 115 6
+Route #41: 40 105
+Route #42: 74 90
+Route #43: 116 117 16
+Route #44: 51 50
+Route #45: 135 48 103
+Route #46: 71 5 25
+Route #47: 65 144
+Route #48: 147 141 133
+Route #49: 13 29 69
+Route #50: 101 80 120
+Route #51: 12 3 119
+Route #52: 38 11 9 24 63
+Route #53: 81
+Route #54: 41 78
+cost: 50733
