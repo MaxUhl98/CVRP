4524732b42bd24ab4f21d3179f87d1e4af0b851f
MaxUhl98 <frozen2ice@web.de>
2024-06-19 11:07:36 +0200

    push relevant files into main

---
+++ solutions/Insertion/cheapest/X-n148-k46.sol
@@ -0,0 +1,54 @@
+Route #1: 99 46 42
+Route #2: 23 47 126
+Route #3: 75 88
+Route #4: 118 62 107
+Route #5: 39 43 89
+Route #6: 44
+Route #7: 86 84
+Route #8: 73 58
+Route #9: 18 137
+Route #10: 110 85 127
+Route #11: 134 109 77
+Route #12: 21 72 124
+Route #13: 136 82 104 142
+Route #14: 68 54 10 129
+Route #15: 122 2 111
+Route #16: 93
+Route #17: 106
+Route #18: 14 98 67 61
+Route #19: 27 22 55
+Route #20: 53 19 4 114 8
+Route #21: 17 112 92
+Route #22: 32 113
+Route #23: 121 140
+Route #24: 83 143 76
+Route #25: 123 128
+Route #26: 56 30
+Route #27: 87 95
+Route #28: 70 35 79
+Route #29: 45 60 94 64
+Route #30: 57 7 130
+Route #31: 96 146
+Route #32: 132 97
+Route #33: 91 139
+Route #34: 26 37 125 52
+Route #35: 31 15 108 74
+Route #36: 49 145 34 36
+Route #37: 28 59
+Route #38: 102 138 131 16
+Route #39: 20 40
+Route #40: 33 66 13 71
+Route #41: 1 5
+Route #42: 100 116 90
+Route #43: 105 6 115
+Route #44: 117 12 3
+Route #45: 51 147
+Route #46: 135 48 103
+Route #47: 65 141
+Route #48: 101 80 120
+Route #49: 38 69 11 50
+Route #50: 25 29 9 24
+Route #51: 81 119
+Route #52: 41 78
+Route #53: 144 133 63
+cost: 50074
